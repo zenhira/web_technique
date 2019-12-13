9e0f46fd2a0438ed7e91eec627e1d0a80073c356
yasuhirozenhira <poseidon.0141@gmail.com>
2019-12-13 16:38:51 +0900

    graphql

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,18 @@
+Types::QueryType = GraphQL::ObjectType.define do
+  name "Query"
+  # Add root-level fields here.
+  # They will be entry points for queries on your schema.
+
+  # TODO: remove me
+  field :testField, types.String do
+    description "An example field added by the generator"
+    resolve ->(obj, args, ctx) {
+      "Hello World!"
+    }
+  end
+  field :blog, !Types::BlogType do
+    resolve ->(_obj, _args, ctx) {
+      ctx[:blog]
+    }
+  end
+end
